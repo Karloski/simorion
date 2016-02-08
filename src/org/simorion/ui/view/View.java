a55d519ba977c51705daf6b119431911456fb8bd
Karl Bown <kmb224@exeter.ac.uk>
2016-02-08 13:26:50 +0000

    First draft of View Interface
    The approximate full implementation of the View interface.

--- src/org/simorion/ui/view/View.java
+++ src/org/simorion/ui/view/View.java
@@ -4,15 +4,26 @@ import java.awt.Color;
 
 import javax.swing.JButton;
 
+/**
+ * View interface which defines methods for a view for the Simori-On. 
+ * @author Karl
+ *
+ */
 public interface View {
 
 	/**
-	 * Retrieves and returns the button at coordinate x, y for this view.
+	 * Retrieves and returns the matrix button at coordinate x, y for this view.
 	 * @param x The x coordinate of the button.
 	 * @param y The y cooridnate of the button.
 	 * @return The matrix JButton at cooridnate x, y.
 	 */
-	JButton getMidiButton(int x, int y);
+	JButton getButton(int x, int y);
+	
+	/**
+	 * Retrieves and returns all matrix buttons for this view.
+	 * @return The buttons for this view.
+	 */
+	JButton[] getButtons();
 	
 	/**
 	 * Retrieves and returns all buttons for the given column for this view.
@@ -29,29 +40,47 @@ public interface View {
 	JButton[] getRow(int x);
 	
 	/**
-	 * Sets the colour of an individual midi button to {@code color} at position x, y for this view.
+	 * Sets the colour of an individual matrix button to {@code color} at position x, y for this view.
 	 * @param x The x coordinate of the matrix button.
 	 * @param y The y coordinate of the matrix button.
 	 * @param color The colour to use.
 	 */
-	void lightMidiButton(int x, int y, Color color);
+	void lightButton(int x, int y, Color color);
 	
 	/**
-	 * Sets the colour of a row of buttons in the button matrix to {@code color} for this view.
-	 * @param row - The row to set.
-	 * @param color - The colour to use.
+	 * Convenience method that sets the colour of a row or column to {@code color} for position {@code position}.
+	 * The {@code position} argument determines the particular column or row to set, and the isRow argument determines
+	 * whether we're setting a column or row.
+	 * The method will set the entire row or column, regardless of the position of the button in the row
+	 * or column.
+	 * @param position The position of the row or column to set.
+	 * @param isRow Whether we're setting a row or column.
+	 * @param color The color to set.
 	 */
-	void lightRow(int row, Color color);
+	void lightButton(int position, boolean isRow, Color color);
 	
 	/**
-	 * Sets the colour of a column of buttons in the button matrix to {@code color} for this view.
-	 * @param column The column to set.
-	 * @param color The colour to use.
+	 * Retrieves and returns whether or not the button at coordinate {@code x}, {@code y} is lit.
+	 * <p>NOTE: I feel like this might be redundant and that the information could be retrieved through
+	 * whatever implemention of the button we use (i.e., put a flag on the button, which can be
+	 * retreved when the getButton method is called).</p>
+	 * @param x The x coordinate of the button.
+	 * @param y The y coordinate of the button.
+	 * @return True if lit, false otherwise.
 	 */
-	void lightColumn(int column, Color color);
+	boolean isLit(int x, int y);
+	
+	/**
+	 * Convenience method to determine if an entire column or row has been lit.
+	 * @param position The column or row to check.
+	 * @param isRow If set to true, will treat position as the row coordinate, otherwise will treat it as a column.
+	 * @return True if the entire column or row is lit, false otherwise.
+	 */
+	boolean isLit(int position, boolean isRow);
 	
 	/**
 	 * Retrieves and returns the current textual output of the LCD for this view.
+	 * @return The textual output of the LCD.
 	 */
 	String getLCDOutput();
 	
@@ -66,10 +95,4 @@ public interface View {
 	 * @return True if this view is the currently active view, false otherwise.
 	 */
 	boolean isActive();
-	
-	/**
-	 * Retrieves and returns the button that was clicked for this view.
-	 * @return The button that was clicked on.
-	 */
-	JButton buttonClicked();
 }
