618aa7e8604260074605665fc747c57c027ce601
Edmund Smith <edmund.pd.smith@gmail.com>
2016-02-09 06:49:35 +0000

    Reflect the reversed direction of information flow

--- src/org/simorion/ui/controller/Controller.java
+++ src/org/simorion/ui/controller/Controller.java
@@ -1,9 +1,20 @@
 package org.simorion.ui.controller;
 
+import org.simorion.ui.model.Model;
 import org.simorion.ui.view.View;
 
 public interface Controller {
 
+	/**
+	 * Get the View associated with the particular device mode
+	 * @return
+	 */
 	public View getView();
+	
+	/**
+	 * Register the model to manipulate
+	 * @param model Model being manipulated
+	 */
+	public void register(Model model); 
 	
 }
