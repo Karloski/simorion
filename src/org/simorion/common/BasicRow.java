04457d1289727200a52b695503bf8a8df81d7cac
Edmund Smith <edmund.pd.smith@gmail.com>
2016-02-10 22:38:41 +0000

    Added Javadocs to classes

--- src/org/simorion/common/BasicRow.java
+++ src/org/simorion/common/BasicRow.java
@@ -1,8 +1,19 @@
 package org.simorion.common;
 
+/**
+ * Basic implementation of a Row and WritableRow. Is backed by an integer, where
+ * each bit corresponds to that cell being on or off
+ * @author Edmund Smith
+ */
 public class BasicRow implements Row, WritableRow {
 
+	/**
+	 * The bitstring representing the cells being on or off
+	 */
 	int lights;
+	/**
+	 * The MIDI note number
+	 */
 	byte note;
 	
 	BasicRow() {
@@ -13,31 +24,49 @@ public class BasicRow implements Row, WritableRow {
 		return (lights & (1 << cell)) == 1;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public byte getNote() {
 		return note;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setLit(int cell) {
 		lights |= 1 << cell;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setUnlit(int cell) {
 		lights &= ~(1 << cell);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void toggleLit(int cell) {
 		lights ^= 1 << cell;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setNote(byte note) {
 		this.note = note;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void applyMask(int and, int or) {
 		lights &= and;
