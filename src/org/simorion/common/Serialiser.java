f561e8974a23f83db51be701108c11a0430c3a70
Karl Bown <kmb224@exeter.ac.uk>
2016-02-11 11:23:53 +0000

    Commenting out control class references

--- src/org/simorion/common/Serialiser.java
+++ src/org/simorion/common/Serialiser.java
@@ -2,8 +2,8 @@ package org.simorion.common;
 
 public interface Serialiser {
 
-	public SerialisedSong serialise(Control c);
+	//public SerialisedSong serialise(Control c);
 	
-	public void deserialise(SerialisedSong s, Control c);
+	//public void deserialise(SerialisedSong s, Control c);
 	
 }
