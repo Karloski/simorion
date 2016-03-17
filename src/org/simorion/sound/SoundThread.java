e335fe7fa05b95e331751badcc0039314589061a
Edmund Smith <edmund.pd.smith@gmail.com>
2016-03-17 11:13:18 +0000

    Preparing for the addendum

--- src/org/simorion/sound/SoundThread.java
+++ src/org/simorion/sound/SoundThread.java
@@ -1,10 +1,16 @@
 
 package org.simorion.sound;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.sound.midi.Instrument;
+import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Receiver;
 import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Soundbank;
 import javax.sound.midi.Synthesizer;
 
 import org.simorion.common.ImmutableLayer;
@@ -39,18 +45,60 @@ public class SoundThread implements Runnable {
 	public MutableModel model;
 	
 	public SoundThread(Song s, MutableModel m) {
+		if (instance != null) {
+			throw new RuntimeException("Cannot have two sound instances!");
+		}
 		song = s;
 		model = m;
+		instance = this;
+	}
+	
+	public static void play(int note) {
+		ShortMessage msg = new ShortMessage();
+	    try {
+			msg.setMessage(ShortMessage.NOTE_ON, 1, instance.model.getCurrentLayer().getVoice().getMidiVoice(), 0);
+		    Synthesizer synth = instance.getSynthesizer();
+		    synth.getChannels()[1].noteOn(note, 75);
+			long tick = synth.getMicrosecondPosition() + (long)(1000000/instance.song.getTempo()) - synth.getLatency();
+		    instance.getSynthesizer().getReceiver().send(msg, tick);
+		    System.out.println("Played "+ note);
+	    } catch (InvalidMidiDataException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MidiUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	private Synthesizer synthesizer;
-	public Synthesizer getSynthesizer() {
+	private static SoundThread instance;
+	
+	static {
+		try {
+			Synthesizer synth = getSynthesizer();
+			synth.unloadAllInstruments(synth.getDefaultSoundbank());
+			Soundbank sb = MidiSystem.getSoundbank(new File("./FluidR3 GM2-2.SF2"));
+			System.out.println(sb.toString());
+			synth.loadAllInstruments(sb);
+			System.out.println("Soundbank Loaded");
+		} catch (InvalidMidiDataException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static Synthesizer synthesizer;
+	private static Synthesizer getSynthesizer() {
 		if(synthesizer == null) try {
 			synthesizer = MidiSystem.getSynthesizer();
 			// this returns an error in the console but is necessary to make sound
 			synthesizer.open();
 		} catch(Exception ex) {
-			System.out.println(ex); System.exit(1);
+			ex.printStackTrace();
+			System.exit(1);
 		}
 		return synthesizer;
 	}
@@ -59,7 +107,9 @@ public class SoundThread implements Runnable {
 		long tick = 0;
 		long tickPlusOne = 0;
 		long nTicks = -1;
+		System.out.println(new File(".").getAbsolutePath());
 		System.out.println("Running song");
+		
 		while (true) {
 			try {
 				Thread.sleep(10);
@@ -81,8 +131,8 @@ public class SoundThread implements Runnable {
 					int midiVoice = layer.getVoice().getMidiVoice();
 					int channel;
 					if (midiVoice <= 128) { 
-				    	Instrument[]  instruments  = synthesizer.getDefaultSoundbank().getInstruments();
-					    synthesizer.loadInstrument(instruments[midiVoice]);
+				    	Instrument[]  instruments  = synthesizer.getLoadedInstruments();
+					    //synthesizer.loadInstrument(instruments[midiVoice]);
 					    msg = new ShortMessage();
 					    msg.setMessage(ShortMessage.PROGRAM_CHANGE, 1, midiVoice, 0);
 					    rcvr.send(msg, tick+layer.getLayerNumber()-synth.getMicrosecondPosition());
